65b7f94adeea8c7936109310d6698f740e84986a
syahfrinorezky <syahfrinorezky@gmail.com>
2025-10-05 05:26:48 +0800

    chore: update change email with session token & add resend otp api call service also

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -17,13 +17,30 @@ export async function registerUser(data: RegisterFormData) {
   return res.json();
 }
 
-export async function verifyOtp(email: string, otp: string) {
+export async function verifyOtp(sessionToken: string, otp: string) {
   const res = await fetch("/api/auth/verify-otp", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ email, otp }),
+    body: JSON.stringify({ sessionToken, otp }),
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message || "Terjadi kesalahan");
+  }
+
+  return res.json();
+}
+
+export async function resendOtp(sessionToken: string) {
+  const res = await fetch("/api/auth/resend-otp", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ sessionToken }),
   });
 
   if (!res.ok) {
