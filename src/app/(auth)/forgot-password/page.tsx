b000c540b9474801c9901c35802db70e00045dd0
syahfrinorezky <syahfrinorezky@gmail.com>
2025-10-09 19:02:52 +0800

    feat: initial forgot pw & reset-pw page

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ForgotPwForm from "@/components/auth/forgotpwform";
+
+export default function ForgotPassword() {
+  return (
+    <div className="min-h-screen flex items-center justify-center w-full">
+      <ForgotPwForm />
+    </div>
+  );
+}
---
+++ src/app/(auth)/reset-password/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ResetPasswordForm from "@/components/auth/resetpwform";
+
+export default function ResetPassword() {
+  return (
+    <div className="min-h-screen flex items-center justify-center w-full">
+      <ResetPasswordForm />
+    </div>
+  );
+}
